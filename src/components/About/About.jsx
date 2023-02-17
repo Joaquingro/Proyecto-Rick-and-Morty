6a78afde04ad7342d0dc25e8e7f6634b9345e36d
Joaquingro <joaquinsgro@gmail.com>
2023-02-16 20:05:14 -0600

    Finalizado

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
     <div className={style.yo}>
         <h2 className={style.back}>Acerca de mi...</h2>
         <img src = 'https://media.tenor.com/doUcitxR2acAAAAM/rick-and-morty-smirk.gif'/>
-        <p className={style.background}>Que tal, soy Joaquin Guerrero, <br/> creador de este 
+        <p className={style.background}>Que tal, soy Joaquin Guerrero :D, <br/> creador de este 
             proyecto de <br/> Rick and Morty.
         </p>
     </div>
